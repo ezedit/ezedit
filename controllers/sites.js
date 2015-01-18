d0bd76c48d06a59e63f03f35e018e538f92ec410
Rahat Ahmed <rahatarmanahmed@gmail.com>
2015-01-18 11:44:01 -0600

    Made client script less wtf

--- controllers/sites.js
+++ controllers/sites.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 var _ = require('underscore');
+var fs = require('fs');
 
 module.exports.getAllSites = function(req, res, next) {
     mongoose.model('site').find(function(err, sites) {
@@ -36,37 +37,10 @@ module.exports.getSiteScript = function(req, res, next) {
         } else {
             var content = _.omit(site.toObject(), ['__v', 'password']);
             console.log(content);
-            var script = 'var content = {';
-            script += 'name: "' + content.name + '", ';
-            script += 'login: "' + content.login + '"';
-
-            if (content.fields !== undefined && content.fields.length !== 0) {
-                script +=', fields: [';
-
-                content.fields.forEach(function(element) {
-                    script += '{';
-                    script += 'name: "' + element.name + '", ';
-                    script += 'description: "' + element.name + '", ';
-                    script += 'body: "' + element.body + '"';
-                    script += '},';
-                });
-
-                // remove trailing comma
-                script = script.slice(0, -1);
-                script += ']';
-            }
-
-            script += '};';
-            script += "\n";
-            script += 'for (var i = 0; i < content.fields.length; i++) {';
-            script += 'var fieldName = content.fields[i].name;';
-            script += 'var fieldBody = content.fields[i].body;';
-            script += 'var selectedFields = document.querySelectorAll(\'[data-ezedit-field="\' + fieldName + \'"]\');';
-            script += 'for (var j = 0; j < selectedFields.length; j++) {';
-            script += 'selectedFields[j].innerHTML = fieldBody;';
-            script += '}';
-            script += '}';
-            res.status(200).send(script);
+            fs.readFile('templates/clientScript.js', function(err, data){
+                console.log(data);
+                res.status(200).send(_.template(data.toString())( content));
+            })
         }
     });
 };
