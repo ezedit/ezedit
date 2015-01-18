e03eda3fd636f99ad7d07b27b75cdb59b6407e34
Zack Urben <zackurben@gmail.com>
2015-01-17 19:09:04 -0600

    Updating api endpoints for testing.

--- routes/site.js
+++ routes/site.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
 
 // GET /site
 router.get('/site', function(req, res, next) {
-  res.send("ok");
+  mongoose.model('site').find(function(err, sites) {
+    if (err) {
+      console.log('Error: ' + err);
+    }
+
+    res.send(sites);
+  });
+});
+
+// GET /site/:id
+router.get('/site/:id', function(req, res, next) {
+  mongoose.model('site').findById(req.params.id, function(err, sites) {
+    if (err) {
+      console.log('Error: ' + err);
+    }
+
+    res.send(sites);
+  });
 });
 
 module.exports = router;
